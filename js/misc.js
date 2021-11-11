2fcca7ac20f899bd3663481df9f835a390af13af
pawel idzikowski <iddziku@gmail.com>
2021-11-11 16:25:46 +0100

    Work on music filter part1

--- js/misc.js
+++ js/misc.js
@@ -1,78 +1,143 @@
-var essays = document.getElementsByClassName('myessay');
+var essays = document.getElementsByClassName("myessay");
+var songs = document.getElementsByClassName("mysong");
 
 $(document).ready(function () {
-    $('.tiny.modal')
-    .modal('hide');
+  $(".tiny.modal").modal("hide");
 
-    $(".activating.element").popup({
-        trigger: "hover",
-        html: true,
-        animation: false
-    });
+  $(".activating.element").popup({
+    trigger: "hover",
+    html: true,
+    animation: false,
+  });
 
-    $('.ui.accordion').accordion({
-        on: null
-    });
+  $(".ui.accordion").accordion({
+    on: null,
+  });
 
-    $('.ui.dropdown').dropdown({
-        maxSelections: 2,
-        onChange: function() {
-            if ($('#labelPicker').val() == null) {
-                displayAllEssays();
-                return false;
+  $(".ui.dropdown").dropdown({
+    maxSelections: 2,
+    onChange: function () {
+      if ($("#labelPicker").val() == null) {
+        displayAllEssays();
+        return false;
+      }
+      var labels = $("#labelPicker").val();
+      // loop through essays
+      for (var i = 0; i < essays.length; i++) {
+        var essayLabels = essays[i].getElementsByClassName("label");
+        var array = [];
+        // loop through essay labels
+        for (var j = 0; j < essayLabels.length; j++) {
+          array.push(essayLabels[j].textContent);
+          var isMatched = false;
+          // loop through picked labels
+          for (var k = 0; k < labels.length; k++) {
+            if (array.includes(labels[k]) == true) {
+              isMatched = true;
+            } else {
+              isMatched = false;
+              break;
             }
-            var labels = $('#labelPicker').val();
-            // loop through essays
-            for (var i = 0; i < essays.length; i++) {
-                var essayLabels = essays[i].getElementsByClassName('label');
-                var array = [];
-                // loop through essay labels
-                for (var j = 0; j < essayLabels.length; j++) {
-                    array.push(essayLabels[j].textContent);
-                    var isMatched = false;
-                    // loop through picked labels
-                    for (var k = 0; k < labels.length; k++) {
-                        if (array.includes(labels[k]) == true) {
-                            isMatched = true;
-                        }
-                        else {
-                            isMatched = false;
-                            break;
-                        }
-                    }
-                }
-                if (isMatched == true) {
-                    essays[i].classList.remove('hide');
-                }
-                else {
-                    essays[i].classList.add('hide');
-                }
-            }
+          }
         }
-    });
+        if (isMatched == true) {
+          essays[i].classList.remove("hide");
+        } else {
+          essays[i].classList.add("hide");
+        }
+      }
+    },
+  });
+  $(".ui.dropdown.music-dropdown").dropdown({
+    onChange: function () {
+      var data = $("#songFilter").val();
+
+      if (data == "") {
+        displayAllSongs();
+
+        return false;
+      }
+
+      // prepare data
+      var pickedAuthors = data
+        .split(",")
+        .flatMap((item) =>
+          item.includes("author") ? item.replace("author: ", "") : []
+        );
+
+      var pickedGenres = data
+        .split(",")
+        .flatMap((item) =>
+          item.includes("genre") ? item.replace("genre: ", "") : []
+        );
+
+      // apply filters
+      for (var i = 0; i < songs.length; i++) {
+        var isMatched = false;
+        var authors = songs[i]
+          .getAttribute("authors")
+          .split(",")
+          .map(function (item) {
+            return item.trim();
+          });
+
+        var genre = songs[i].getAttribute("genre");
+
+        if (pickedAuthors) {
+          for (var j = 0; j < authors.length; j++) {
+            if (pickedAuthors.includes(authors[j])) {
+              isMatched = true;
+              break;
+            }
+          }
+        }
+
+        if (pickedGenres[0]) {
+          console.log("check");
+          console.log(pickedGenres);
+          console.log(genre);
+          console.log(pickedGenres.includes(genre));
+          isMatched = pickedGenres.includes(genre);
+        }
+
+        if (!isMatched) {
+          songs[i].classList.add("hide");
+        } else {
+          songs[i].classList.remove("hide");
+        }
+      }
+    },
+  });
 });
 
 function displayAllEssays() {
-    for (var i = 0; i < essays.length; i++) { 
-        essays[i].classList.remove('hide');
-    }
+  for (var i = 0; i < essays.length; i++) {
+    essays[i].classList.remove("hide");
+  }
+}
+
+function displayAllSongs() {
+  for (var i = 0; i < songs.length; i++) {
+    songs[i].classList.remove("hide");
+  }
 }
 
 function test() {
-    if (document.images) {
-        alert('kapp');
-    }
+  if (document.images) {
+    alert("kapp");
+  }
 }
 
 function showModalWithImage(container) {
-    document.getElementById('imgPlaceholder').src = container.children[0].src;
-    $('.tiny.modal')
-    .modal('show');
+  document.getElementById("imgPlaceholder").src = container.children[0].src;
+  $(".tiny.modal").modal("show");
 }
 
 function copyToClipboard() {
-  var copyText = document.getElementById('hexStringPlaceholder').textContent.trim();
-  var tempInput = document.createElement('input');
+  var copyText = document
+    .getElementById("hexStringPlaceholder")
+    .textContent.trim();
+  var tempInput = document.createElement("input");
   tempInput.value = copyText;
   document.body.appendChild(tempInput);
   tempInput.select();
@@ -82,12 +147,9 @@ function copyToClipboard() {
 }
 
 function redirectBlank(url) {
-    window.open(
-        url,
-        '_blank'
-      );
+  window.open(url, "_blank");
 }
 
 function redirect(url) {
-    window.location = url;
+  window.location = url;
 }
