161eeb71fc45fc0f53b5417da741bf4fa906cc74
pawel idzikowski <iddziku@gmail.com>
2022-08-21 16:29:32 +0200

    Remove bouncing effect from avatar element

--- js/particles/jquery.particleground.js
+++ js/particles/jquery.particleground.js
@@ -8,9 +8,9 @@
  * Inspired by http://requestlab.fr/ and http://disruptivebydesign.com/
  */
 
-; (function (window, document) {
+(function (window, document) {
   "use strict";
-  var pluginName = 'particleground';
+  var pluginName = "particleground";
 
   // http://youmightnotneedjquery.com/#deep_extend
   function extend(out) {
@@ -20,36 +20,18 @@
       if (!obj) continue;
       for (var key in obj) {
         if (obj.hasOwnProperty(key)) {
-          if (typeof obj[key] === 'object')
-            deepExtend(out[key], obj[key]);
-          else
-            out[key] = obj[key];
+          if (typeof obj[key] === "object") deepExtend(out[key], obj[key]);
+          else out[key] = obj[key];
         }
       }
     }
     return out;
-  };
+  }
 
   var $ = window.jQuery;
 
-  var avatar;
-  var avatarRectTop;
-  var avatarRectRight;
-  var avatarRectBottom;
-  var avatarRectLeft;
-
-  $(window).load(function () {
-    avatar = document.getElementById('avatar');
-    var rect = avatar.getBoundingClientRect();
-    avatarRectTop = rect.top - 10;
-    avatarRectRight = rect.right;
-    avatarRectBottom = rect.bottom + 10;
-    avatarRectLeft = rect.left;
-  });
-
-
   function Plugin(element, options) {
-    var canvasSupport = !!document.createElement('canvas').getContext;
+    var canvasSupport = !!document.createElement("canvas").getContext;
     var canvas;
     var ctx;
     var particles = [];
@@ -58,7 +40,9 @@
     var mouseY = 0;
     var winW;
     var winH;
-    var desktop = !navigator.userAgent.match(/(iPhone|iPod|iPad|Android|BlackBerry|BB10|mobi|tablet|opera mini|nexus 7)/i);
+    var desktop = !navigator.userAgent.match(
+      /(iPhone|iPod|iPad|Android|BlackBerry|BB10|mobi|tablet|opera mini|nexus 7)/i
+    );
     var orientationSupport = !!window.DeviceOrientationEvent;
     var tiltX = 0;
     var pointerX;
@@ -72,27 +56,35 @@
      * Init
      */
     function init() {
-      if (!canvasSupport) { return; }
+      if (!canvasSupport) {
+        return;
+      }
 
       //Create canvas
-      canvas = document.createElement('canvas');
-      canvas.className = 'pg-canvas';
-      canvas.style.display = 'block';
+      canvas = document.createElement("canvas");
+      canvas.className = "pg-canvas";
+      canvas.style.display = "block";
       element.insertBefore(canvas, element.firstChild);
-      ctx = canvas.getContext('2d');
+      ctx = canvas.getContext("2d");
       styleCanvas();
 
       // Create particles
-      var numParticles = Math.round((canvas.width * canvas.height) / options.density);
+      var numParticles = Math.round(
+        (canvas.width * canvas.height) / options.density
+      );
       for (var i = 0; i < numParticles; i++) {
         var p = new Particle();
         p.setStackPos(i);
         particles.push(p);
-      };
+      }
 
-      window.addEventListener('resize', function () {
-        resizeHandler();
-      }, false);
+      window.addEventListener(
+        "resize",
+        function () {
+          resizeHandler();
+        },
+        false
+      );
 
       // On mouse move react func
       /*
@@ -103,15 +95,19 @@
       */
 
       if (orientationSupport && !desktop) {
-        window.addEventListener('deviceorientation', function () {
-          // Contrain tilt range to [-30,30]
-          tiltY = Math.min(Math.max(-event.beta, -30), 30);
-          tiltX = Math.min(Math.max(-event.gamma, -30), 30);
-        }, true);
+        window.addEventListener(
+          "deviceorientation",
+          function () {
+            // Contrain tilt range to [-30,30]
+            tiltY = Math.min(Math.max(-event.beta, -30), 30);
+            tiltX = Math.min(Math.max(-event.gamma, -30), 30);
+          },
+          true
+        );
       }
 
       draw();
-      hook('onInit');
+      hook("onInit");
     }
 
     /**
@@ -129,7 +125,9 @@
      * Draw particles
      */
     function draw() {
-      if (!canvasSupport) { return; }
+      if (!canvasSupport) {
+        return;
+      }
 
       winW = window.innerWidth;
       winH = window.innerHeight;
@@ -140,11 +138,11 @@
       // Update particle positions
       for (var i = 0; i < particles.length; i++) {
         particles[i].updatePosition();
-      };
+      }
       // Draw particles
       for (var i = 0; i < particles.length; i++) {
         particles[i].draw();
-      };
+      }
 
       // Call this function next time screen is redrawn
       if (!paused) {
@@ -164,13 +162,18 @@
 
       // Remove particles that are outside the canvas
       for (var i = particles.length - 1; i >= 0; i--) {
-        if (particles[i].position.x > elWidth || particles[i].position.y > elHeight) {
+        if (
+          particles[i].position.x > elWidth ||
+          particles[i].position.y > elHeight
+        ) {
           particles.splice(i, 1);
         }
-      };
+      }
 
       // Adjust particle density
-      var numParticles = Math.round((canvas.width * canvas.height) / options.density);
+      var numParticles = Math.round(
+        (canvas.width * canvas.height) / options.density
+      );
       if (numParticles > particles.length) {
         while (numParticles > particles.length) {
           var p = new Particle();
@@ -183,7 +186,7 @@
       // Re-index particles
       for (i = particles.length - 1; i >= 0; i--) {
         particles[i].setStackPos(i);
-      };
+      }
     }
 
     /**
@@ -201,24 +204,6 @@
       draw();
     }
 
-    function randomizeSpawnPosition() {
-      var x = 200;
-      var y = 200;
-
-      while (true) {
-        x = Math.ceil(Math.random() * canvas.width);
-        y = Math.ceil(Math.random() * canvas.height);
-
-        if ((x > avatarRectLeft && x < avatarRectRight) || (y > avatarRectTop && y < avatarRectBottom)) {
-          continue;
-        }
-
-        break;
-      }
-
-      return [x, y];
-    }
-
     /**
      * Particle
      */
@@ -228,38 +213,57 @@
       this.layer = Math.ceil(Math.random() * 3);
       this.parallaxOffsetX = 0;
       this.parallaxOffsetY = 0;
-
       // Initial particle position
-      var intialPosition = randomizeSpawnPosition();
-
       this.position = {
-        x: intialPosition[0],
-        y: intialPosition[1]
-      }
+        x: Math.ceil(Math.random() * canvas.width),
+        y: Math.ceil(Math.random() * canvas.height),
+      };
       // Random particle speed, within min and max values
-      this.speed = {}
+      this.speed = {};
       switch (options.directionX) {
-        case 'left':
-          this.speed.x = +(-options.maxSpeedX + (Math.random() * options.maxSpeedX) - options.minSpeedX).toFixed(2);
+        case "left":
+          this.speed.x = +(
+            -options.maxSpeedX +
+            Math.random() * options.maxSpeedX -
+            options.minSpeedX
+          ).toFixed(2);
           break;
-        case 'right':
-          this.speed.x = +((Math.random() * options.maxSpeedX) + options.minSpeedX).toFixed(2);
+        case "right":
+          this.speed.x = +(
+            Math.random() * options.maxSpeedX +
+            options.minSpeedX
+          ).toFixed(2);
           break;
         default:
-          this.speed.x = +((-options.maxSpeedX / 2) + (Math.random() * options.maxSpeedX)).toFixed(2);
-          this.speed.x += this.speed.x > 0 ? options.minSpeedX : -options.minSpeedX;
+          this.speed.x = +(
+            -options.maxSpeedX / 2 +
+            Math.random() * options.maxSpeedX
+          ).toFixed(2);
+          this.speed.x +=
+            this.speed.x > 0 ? options.minSpeedX : -options.minSpeedX;
           break;
       }
       switch (options.directionY) {
-        case 'up':
-          this.speed.y = +(-options.maxSpeedY + (Math.random() * options.maxSpeedY) - options.minSpeedY).toFixed(2);
+        case "up":
+          this.speed.y = +(
+            -options.maxSpeedY +
+            Math.random() * options.maxSpeedY -
+            options.minSpeedY
+          ).toFixed(2);
           break;
-        case 'down':
-          this.speed.y = +((Math.random() * options.maxSpeedY) + options.minSpeedY).toFixed(2);
+        case "down":
+          this.speed.y = +(
+            Math.random() * options.maxSpeedY +
+            options.minSpeedY
+          ).toFixed(2);
           break;
         default:
-          this.speed.y = +((-options.maxSpeedY / 2) + (Math.random() * options.maxSpeedY)).toFixed(2);
-          this.speed.x += this.speed.y > 0 ? options.minSpeedY : -options.minSpeedY;
+          this.speed.y = +(
+            -options.maxSpeedY / 2 +
+            Math.random() * options.maxSpeedY
+          ).toFixed(2);
+          this.speed.x +=
+            this.speed.y > 0 ? options.minSpeedY : -options.minSpeedY;
           break;
       }
     }
@@ -270,7 +274,14 @@
     Particle.prototype.draw = function () {
       // Draw circle
       ctx.beginPath();
-      ctx.arc(this.position.x + this.parallaxOffsetX, this.position.y + this.parallaxOffsetY, options.particleRadius / 2, 0, Math.PI * 2, true);
+      ctx.arc(
+        this.position.x + this.parallaxOffsetX,
+        this.position.y + this.parallaxOffsetY,
+        options.particleRadius / 2,
+        0,
+        Math.PI * 2,
+        true
+      );
       ctx.closePath();
       ctx.fill();
 
@@ -281,23 +292,34 @@
         var p2 = particles[i];
 
         // Pythagorus theorum to get distance between two points
-        var a = this.position.x - p2.position.x
-        var b = this.position.y - p2.position.y
-        var dist = Math.sqrt((a * a) + (b * b)).toFixed(2);
+        var a = this.position.x - p2.position.x;
+        var b = this.position.y - p2.position.y;
+        var dist = Math.sqrt(a * a + b * b).toFixed(2);
 
         // If the two particles are in proximity, join them
         if (dist < options.proximity) {
-          ctx.moveTo(this.position.x + this.parallaxOffsetX, this.position.y + this.parallaxOffsetY);
+          ctx.moveTo(
+            this.position.x + this.parallaxOffsetX,
+            this.position.y + this.parallaxOffsetY
+          );
           if (options.curvedLines) {
-            ctx.quadraticCurveTo(Math.max(p2.position.x, p2.position.x), Math.min(p2.position.y, p2.position.y), p2.position.x + p2.parallaxOffsetX, p2.position.y + p2.parallaxOffsetY);
+            ctx.quadraticCurveTo(
+              Math.max(p2.position.x, p2.position.x),
+              Math.min(p2.position.y, p2.position.y),
+              p2.position.x + p2.parallaxOffsetX,
+              p2.position.y + p2.parallaxOffsetY
+            );
           } else {
-            ctx.lineTo(p2.position.x + p2.parallaxOffsetX, p2.position.y + p2.parallaxOffsetY);
+            ctx.lineTo(
+              p2.position.x + p2.parallaxOffsetX,
+              p2.position.y + p2.parallaxOffsetY
+            );
           }
         }
       }
       ctx.stroke();
       ctx.closePath();
-    }
+    };
 
     /**
      * update particle position
@@ -316,62 +338,61 @@
           pointerY = mouseY;
         }
         // Calculate parallax offsets
-        this.parallaxTargX = (pointerX - (winW / 2)) / (options.parallaxMultiplier * this.layer);
-        this.parallaxOffsetX += (this.parallaxTargX - this.parallaxOffsetX) / 10; // Easing equation
-        this.parallaxTargY = (pointerY - (winH / 2)) / (options.parallaxMultiplier * this.layer);
-        this.parallaxOffsetY += (this.parallaxTargY - this.parallaxOffsetY) / 10; // Easing equation
+        this.parallaxTargX =
+          (pointerX - winW / 2) / (options.parallaxMultiplier * this.layer);
+        this.parallaxOffsetX +=
+          (this.parallaxTargX - this.parallaxOffsetX) / 10; // Easing equation
+        this.parallaxTargY =
+          (pointerY - winH / 2) / (options.parallaxMultiplier * this.layer);
+        this.parallaxOffsetY +=
+          (this.parallaxTargY - this.parallaxOffsetY) / 10; // Easing equation
       }
 
       var elWidth = element.offsetWidth;
       var elHeight = element.offsetHeight;
 
       switch (options.directionX) {
-        case 'left':
+        case "left":
           if (this.position.x + this.speed.x + this.parallaxOffsetX < 0) {
             this.position.x = elWidth - this.parallaxOffsetX;
-          } else if (this.position.x + this.speed.x + this.parallaxOffsetX < avatarRectLeft) {
-            console.log('fuf');
-            this.position.x = avatarRectLeft - this.parallaxOffsetX;
           }
           break;
-        case 'right':
+        case "right":
           if (this.position.x + this.speed.x + this.parallaxOffsetX > elWidth) {
             this.position.x = 0 - this.parallaxOffsetX;
           }
           break;
         default:
           // If particle has reached edge of canvas, reverse its direction
-          const particleXPos = this.position.x + this.speed.x + this.parallaxOffsetX;
-          const particleYPos = this.position.y + this.speed.y + this.parallaxOffsetY;
-
-          if (this.position.x + this.speed.x + this.parallaxOffsetX > elWidth || this.position.x + this.speed.x + this.parallaxOffsetX < 0) {
-            this.speed.x = -this.speed.x;
-          } else if (particleXPos < avatarRectRight && particleXPos > avatarRectLeft && particleYPos > avatarRectTop && particleYPos < avatarRectBottom) {
+          if (
+            this.position.x + this.speed.x + this.parallaxOffsetX > elWidth ||
+            this.position.x + this.speed.x + this.parallaxOffsetX < 0
+          ) {
             this.speed.x = -this.speed.x;
           }
           break;
       }
 
       switch (options.directionY) {
-        case 'up':
+        case "up":
           if (this.position.y + this.speed.y + this.parallaxOffsetY < 0) {
             this.position.y = elHeight - this.parallaxOffsetY;
           }
           break;
-        case 'down':
-          if (this.position.y + this.speed.y + this.parallaxOffsetY > elHeight) {
+        case "down":
+          if (
+            this.position.y + this.speed.y + this.parallaxOffsetY >
+            elHeight
+          ) {
             this.position.y = 0 - this.parallaxOffsetY;
           }
           break;
         default:
-          const particleXPos = this.position.x + this.speed.x + this.parallaxOffsetX;
-          const particleYPos = this.position.y + this.speed.y + this.parallaxOffsetY;
-
           // If particle has reached edge of canvas, reverse its direction
-          if (this.position.y + this.speed.y + this.parallaxOffsetY > elHeight || this.position.y + this.speed.y + this.parallaxOffsetY < 0) {
-            this.speed.y = -this.speed.y;
-          } else if (particleYPos > avatarRectTop && particleYPos < avatarRectBottom && particleXPos > avatarRectLeft && particleXPos < avatarRectRight) {
-            this.speed.x = -this.speed.x;
+          if (
+            this.position.y + this.speed.y + this.parallaxOffsetY > elHeight ||
+            this.position.y + this.speed.y + this.parallaxOffsetY < 0
+          ) {
             this.speed.y = -this.speed.y;
           }
           break;
@@ -380,14 +401,14 @@
       // Move particle
       this.position.x += this.speed.x;
       this.position.y += this.speed.y;
-    }
+    };
 
     /**
      * Setter: particle stacking position
      */
     Particle.prototype.setStackPos = function (i) {
       this.stackPos = i;
-    }
+    };
 
     function option(key, val) {
       if (val) {
@@ -398,11 +419,11 @@
     }
 
     function destroy() {
-      console.log('destroy');
+      console.log("destroy");
       canvas.parentNode.removeChild(canvas);
-      hook('onDestroy');
+      hook("onDestroy");
       if ($) {
-        $(element).removeData('plugin_' + pluginName);
+        $(element).removeData("plugin_" + pluginName);
       }
     }
 
@@ -418,7 +439,7 @@
       option: option,
       destroy: destroy,
       start: start,
-      pause: pause
+      pause: pause,
     };
   }
 
@@ -428,34 +449,44 @@
 
   window[pluginName].defaults = {
     minSpeedX: 0.1,
-    maxSpeedX: 0.7,
+    maxSpeedX: 0.2,
     minSpeedY: 0.1,
-    maxSpeedY: 0.7,
-    directionX: 'center', // 'center', 'left' or 'right'. 'center' = dots bounce off edges
-    directionY: 'center', // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+    maxSpeedY: 0.2,
     density: 10000, // How many particles will be generated: one particle every n pixels
-    dotColor: '#666666',
-    lineColor: '#666666',
+    curvedLines: false,
+    parallax: true,
+    directionX: "center", // 'center', 'left' or 'right'. 'center' = dots bounce off edges
+    directionY: "center", // 'center', 'up' or 'down'. 'center' = dots bounce off edges
+    density: 10000, // How many particles will be generated: one particle every n pixels
+    dotColor: "#666666",
+    lineColor: "#666666",
     particleRadius: 4, // Dot size
     lineWidth: 0.2,
     curvedLines: false,
     proximity: 80, // How close two dots need to be before they join
     parallax: true,
     parallaxMultiplier: 10, // The lower the number, the more extreme the parallax effect
-    onInit: function () { },
-    onDestroy: function () { }
+    onInit: function () {},
+    onDestroy: function () {},
   };
 
   // nothing wrong with hooking into jQuery if it's there...
   if ($) {
     $.fn[pluginName] = function (options) {
-      if (typeof arguments[0] === 'string') {
+      if (typeof arguments[0] === "string") {
         var methodName = arguments[0];
         var args = Array.prototype.slice.call(arguments, 1);
         var returnVal;
         this.each(function () {
-          if ($.data(this, 'plugin_' + pluginName) && typeof $.data(this, 'plugin_' + pluginName)[methodName] === 'function') {
-            returnVal = $.data(this, 'plugin_' + pluginName)[methodName].apply(this, args);
+          if (
+            $.data(this, "plugin_" + pluginName) &&
+            typeof $.data(this, "plugin_" + pluginName)[methodName] ===
+              "function"
+          ) {
+            returnVal = $.data(this, "plugin_" + pluginName)[methodName].apply(
+              this,
+              args
+            );
           }
         });
         if (returnVal !== undefined) {
@@ -465,14 +496,13 @@
         }
       } else if (typeof options === "object" || !options) {
         return this.each(function () {
-          if (!$.data(this, 'plugin_' + pluginName)) {
-            $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+          if (!$.data(this, "plugin_" + pluginName)) {
+            $.data(this, "plugin_" + pluginName, new Plugin(this, options));
           }
         });
       }
     };
   }
-
 })(window, document);
 
 /**
@@ -483,19 +513,21 @@
  */
 (function () {
   var lastTime = 0;
-  var vendors = ['ms', 'moz', 'webkit', 'o'];
+  var vendors = ["ms", "moz", "webkit", "o"];
   for (var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-    window.requestAnimationFrame = window[vendors[x] + 'RequestAnimationFrame'];
-    window.cancelAnimationFrame = window[vendors[x] + 'CancelAnimationFrame']
-      || window[vendors[x] + 'CancelRequestAnimationFrame'];
+    window.requestAnimationFrame = window[vendors[x] + "RequestAnimationFrame"];
+    window.cancelAnimationFrame =
+      window[vendors[x] + "CancelAnimationFrame"] ||
+      window[vendors[x] + "CancelRequestAnimationFrame"];
   }
 
   if (!window.requestAnimationFrame)
     window.requestAnimationFrame = function (callback, element) {
       var currTime = new Date().getTime();
       var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-      var id = window.setTimeout(function () { callback(currTime + timeToCall); },
-        timeToCall);
+      var id = window.setTimeout(function () {
+        callback(currTime + timeToCall);
+      }, timeToCall);
       lastTime = currTime + timeToCall;
       return id;
     };
@@ -504,4 +536,4 @@
     window.cancelAnimationFrame = function (id) {
       clearTimeout(id);
     };
-}());
+})();
